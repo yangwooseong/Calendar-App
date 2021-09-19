f9140c86f2d18612a99cd8d3ce21daa51c1e34c2
Wooseong Yang/양우성 <83999003+YangWooseong-vuno@users.noreply.github.com>
2021-09-19 23:00:41 +0900

    prac: express

---
+++ warm-up/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 2017,
+  },
+  env: {
+    es6: true,
+    node: true,
+  },
+}
---
+++ warm-up/api/hello.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+
+let a = 1
+// An example middleware function
+let a_middleware_function = function (req, res, next) {
+  // ... perform some operations
+  console.log('called')
+  a += 1
+  next() // Call next() so Express will call the next middleware function in the chain.
+}
+
+// Function added with use() for a specific route
+app.use('/someroute', a_middleware_function)
+
+app.get('/', function (req, res) {
+  console.log(a)
+})
+app.get('/someroute', function (req, res) {
+  res.send('some route called')
+  console.log('some route called')
+  const a = new Promise((res, rej) => {
+    res('hi')
+  }).then((res) => {
+    throw new Error('rror') // built-in 된 error handling 이 있어서 맨 마지막에 실행됨 -> stack trace
+  })
+})
+app.use(express.static('assets'))
+app.listen(3000)
