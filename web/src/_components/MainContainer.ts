7131d93a88bc4270ac5ee5f3b6f817d1ef9f711c
Wooseong Yang/양우성 <83999003+YangWooseong-vuno@users.noreply.github.com>
2021-09-20 21:38:28 +0900

    add calendar content layout

--- web/src/_components/MainContainer.ts
+++ web/src/_components/MainContainer.ts
@@ -1,14 +1,38 @@
 import styles from './MainContainer.module.scss'
 import classNames from 'classnames/bind'
+const cx = classNames.bind(styles)
 
 export default class MainContainer {
   constructor($target: HTMLElement) {
     const calendar = document.createElement('section')
-    const cx = classNames.bind(styles)
     calendar.className = cx('calendar')
-    $target.appendChild(calendar)
+    this.addCalendarHeader(calendar)
+    this.addCalendarContent(calendar)
 
-    console.log(document.querySelector(`.${cx('main-page')}`))
-    calendar.innerText = 'ha'
+    $target.appendChild(calendar)
+  }
+
+  addCalendarHeader($target: HTMLElement) {
+    const arr = ['일', '월', '화', '수', '목', '금', '토']
+    const dayHeader = document.createElement('header')
+    dayHeader.className = cx('calendar-header')
+    $target.appendChild(dayHeader)
+    arr.forEach((day, idx) => {
+      const span: HTMLSpanElement = document.createElement('span')
+      span.innerText = day
+      dayHeader.appendChild(span)
+    })
+  }
+
+  addCalendarContent($target: HTMLElement) {
+    const content = document.createElement('section')
+    content.className = cx('calendar-content')
+    $target.appendChild(content)
+    for (let i = 0; i < 35; i++) {
+      const td = document.createElement('span')
+      td.className = cx('td')
+      td.innerText = i.toString()
+      content.appendChild(td)
+    }
   }
 }
