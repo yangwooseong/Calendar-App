d79526da6d07776b870e1aefda974805a6731d86
Wooseong Yang/양우성 <dntjddid1@gmail.com>
2021-09-25 23:23:08 +0900

    add header click handling

--- web/src/_components/Header.ts
+++ web/src/_components/Header.ts
@@ -1,16 +1,45 @@
 import RangeSelctor from './RangeSelector'
 import TimeRange from './TimeRange'
-import styles from './Header.module.scss'
-import classNames from 'classnames/bind'
+import { IMonthAndYear } from '../_interfaces/IMonthAndYear'
 
 export default class Header {
-  constructor($target: HTMLElement) {
-    const nav: HTMLElement = document.createElement('header')
-    const cx = classNames.bind(styles)
-    nav.className = cx('header')
+  monthAndYear: IMonthAndYear
+  handleClick: (e: Event) => void
+  nav: HTMLHeadElement
+  $target: HTMLElement
+  constructor(
+    $target: HTMLElement,
+    monthAndYear: IMonthAndYear,
+    handleClick: (e: Event) => void
+  ) {
+    const nav: HTMLHeadElement = document.createElement('header')
+    nav.className = 'header'
     $target.appendChild(nav)
 
-    const timeRange = new TimeRange(nav)
-    const rangeSelector = new RangeSelctor(nav)
+    this.monthAndYear = monthAndYear
+    this.handleClick = handleClick
+    this.nav = nav
+    this.$target = $target
+
+    this.render()
+  }
+
+  template() {
+    this.nav.innerHTML = `
+    <div class='time-range'></div>
+    <div class='range-selector'></div>
+    `
+  }
+
+  render() {
+    this.template()
+
+    const nav: HTMLHeadElement = this.$target.querySelector('.header')!
+    const timeRange: HTMLDivElement = this.$target.querySelector('.time-range')!
+    const RangeSelector: HTMLDivElement =
+      this.$target.querySelector('.range-selector')!
+
+    new TimeRange(timeRange, this.monthAndYear, this.handleClick)
+    // new RangeSelctor(nav)
   }
 }
--- web/src/_components/MainContainer.ts
+++ web/src/_components/MainContainer.ts
@@ -1,15 +1,16 @@
 import styles from './MainContainer.module.scss'
 import classNames from 'classnames/bind'
 import OneDay from './OneDay'
+import { IMonthAndYear } from '../_interfaces/IMonthAndYear'
 
 const cx = classNames.bind(styles)
-
 export default class MainContainer {
-  month: number
-  constructor($target: HTMLElement, month: number) {
+  monthAndYear: IMonthAndYear
+
+  constructor($target: HTMLElement, monthAndYear: IMonthAndYear) {
     const calendar = document.createElement('section')
     calendar.className = cx('calendar')
-    this.month = month
+    this.monthAndYear = monthAndYear
     this.addCalendarHeader(calendar)
     this.addCalendarContent(calendar)
 
@@ -32,19 +33,9 @@ export default class MainContainer {
     const content = document.createElement('section')
     content.className = cx('calendar-content')
     $target.appendChild(content)
-    // const year = 2021
-    // const month = moment().month().toString()
-    // const firstDay = moment([year, month, 1])
-    // const lastDate = moment([year, month, 1])
-    //   .add('months', 1)
-    //   .subtract('days', 1)
-    //   .date()
-    // const firstDateOfNextMonth = console.log(lastDate)
-    // const day = firstDay.day()
-    // const lastSundayOfLastMonth = firstDay.subtract('days', day).date()
-    const year = 2021
-    const date = new Date()
-    const month = date.getMonth()
+
+    const year = this.monthAndYear.year
+    const month = this.monthAndYear.month
     const firstDate = new Date(year, month, 1)
     const lastDate = new Date(year, month + 1, 0).getDate()
     const dayOfFirstDate = firstDate.getDay()
@@ -80,5 +71,9 @@ export default class MainContainer {
         ('0' + i.toString()).slice(-2)
       new OneDay(content, dateTime)
     }
+  }
+
+  displayPlan() {
+    console.log('here')
   }
 }
